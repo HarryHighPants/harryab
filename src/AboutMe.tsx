3adb6bee103d98320f5434244002bde17a7ee17f
Harrison Armitage <38173749+HarryHighPants@users.noreply.github.com>
2024-03-21 23:24:03 +1000

    Basic page layout

---
+++ src/AboutMe.tsx
@@ -0,0 +1,14 @@
+const AboutMe = () => {
+  return (
+    <div className="flex-1 flex-col">
+      <h1>Hey</h1>
+      <p>
+        I'm Harry AB a software dev in Brisbane, Australia. I have a passion for
+        solving real-life problems by utilising my technical and creative
+        skills.
+      </p>
+    </div>
+  )
+}
+
+export default AboutMe
---
+++ src/FeaturedProject.tsx
@@ -0,0 +1,10 @@
+const FeaturedProject = () => {
+  return (
+    <div className="flex-1 bg-green-900">
+      <h1>Featured Project</h1>
+      <p>My featured project is the one I'm working on right now!</p>
+    </div>
+  )
+}
+
+export default FeaturedProject
