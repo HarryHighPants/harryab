c5660d4847ec9625b9105833b71cdc4b4a24caae
Harrison Armitage <38173749+HarryHighPants@users.noreply.github.com>
2024-03-30 11:12:13 +1000

    Update colours

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,6 +1,6 @@
 const AboutMe = () => {
   return (
-    <div className="flex-1 p-16 max-h-[100vh] -mt-1">
+    <div className="flex-1 p-16 max-h-[100vh] bg-gray-900 text-white">
       <h1>Hey</h1>
       <p>
         I'm Harry AB a software dev in Brisbane, Australia. I have a passion for
